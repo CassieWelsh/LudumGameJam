bb4a5e6276d6f166aa0a0452725770e816fe1835
Cassie Welsh <maksim.airapetyan3@yandex.ru>
2022-04-02 12:08:37 +0300

    Added Player prefab and edited movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,28 +11,42 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rgbody;
     private Vector2 moveDirection = Vector2.zero;
     [SerializeField]
-    private InputAction movementControls;
+    private PlayerInput playerInput;
+    private InputAction move;
+    private InputAction fire;
 
     void OnEnable()
     {
-        movementControls.Enable();
+        move = playerInput.Player.Move;
+        move.Enable();
+
+        fire = playerInput.Player.Fire;
+        fire.Enable();
+        fire.performed += Fire;
     }    
 
     void OnDisable()
     {
-        movementControls.Disable();
+        move.Disable();
+        fire.Disable();
     }    
 
     void Awake()
     {
         rgbody = GetComponent<Rigidbody2D>();
+        playerInput = new PlayerInput();
     }
 
     void Update()
     {
-        moveDirection = movementControls.ReadValue<Vector2>();        
+        moveDirection = move.ReadValue<Vector2>();        
         rgbody.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
         if(moveDirection.x == 0 && moveDirection.y == 0)
             rgbody.velocity -= new Vector2(0, fallingSpeed);
+    }
+
+    private void Fire(InputAction.CallbackContext context)
+    {
+        print("Pew");
     }
 }
