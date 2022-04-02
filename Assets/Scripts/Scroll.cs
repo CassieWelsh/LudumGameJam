1849878916c363e43b6bede37518d060ed2a57c7
Huntsman <huntsman2411@gmail.com>
2022-04-02 20:37:39 +0300

    Scroll Script

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+
+    public float speed = 0.5f;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        Vector2 offset = new Vector2(0, Time.time * speed);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
