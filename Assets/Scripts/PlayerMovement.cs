f6ef2d92b0c87217159c6dfaa08372e1d756d542
Cassie Welsh <maksim.airapetyan3@yandex.ru>
2022-04-02 20:20:26 +0300

    Fixed Input system issues

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -51,14 +51,13 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         //Player movement
-        this._moveValue = _movement.ReadValue<Vector2>();
-        // Vector2 _moveValue = Vector2.SmoothDamp(_currentMoveValue, this._moveValue, ref _smoothInputVelocity, _smoothInputSpeed);
-        _playerTransform.position += _playerTransform.up * speed * Time.deltaTime * _moveValue.y;
-        print(_moveValue);
+        _moveValue = _movement.ReadValue<Vector2>();        
+        Vector2 _actualMoveValue = Vector2.SmoothDamp(_currentMoveValue, _moveValue, ref _smoothInputVelocity, _smoothInputSpeed);
+        _playerTransform.position += _playerTransform.up * speed * Time.deltaTime * _actualMoveValue.y;
 
-        _playerTransform.Rotate(new Vector3(0, 0, -rotationSpeed * this._moveValue.x * Time.deltaTime));
+        _playerTransform.Rotate(new Vector3(0, 0, -rotationSpeed * _moveValue.x * Time.deltaTime));
 
-        if (this._moveValue.y == 0)
+        if (_moveValue.y == 0)
             _playerTransform.position -= new Vector3(0, fallingSpeed * Time.deltaTime, 0);
         
         //Canon follows the mouse
