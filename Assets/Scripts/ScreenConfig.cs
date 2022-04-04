3421aea2b0bee5d0237d4ad749a31745a2abb00d
Cassie Welsh <maksim.airapetyan3@yandex.ru>
2022-04-04 22:25:09 +0300

    Minor bug fixes

--- Assets/Scripts/ScreenConfig.cs
+++ Assets/Scripts/ScreenConfig.cs
@@ -2,21 +2,8 @@ using UnityEngine;
 
 public class ScreenConfig : MonoBehaviour
 {
-    [Tooltip("Aspect Ratio to use for game.  If Vector2.zero, the default aspect ratio will be used.")]
-    [SerializeField]
-    private Vector2 aspectRatio = Vector2.zero;
-
-    [Tooltip("Whether or not full screen will be used")]
-    [SerializeField]
-    private bool fullScreen = false;
-
-    private void Awake()
+    void Start()
     {
-        if(aspectRatio != Vector2.zero)
-        {
-            float x = Screen.height * (aspectRatio.x / aspectRatio.y);
-            float y = Screen.height;
-            Screen.SetResolution((int) x, (int) y, fullScreen);
-        }
+        Application.targetFrameRate = 60;
     }
 }
